d7da31259102b7220f54339db0d201337eeaeec4
Kashif Saeed <kashif@kashif.(none)>
2012-05-11 17:41:31 +0500

    first commit for a rails project this includes database.yml, which will be removed later

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNruyby::Application.config.secret_token = '61eb7429debe8ca334e5085182f242e9a4bbdd1b93b5e2f852af3184b5e42084307d4fbcd4e31bcce889f756549c686cf3745c0ea04134d9aa2a15f95a3d9897'
